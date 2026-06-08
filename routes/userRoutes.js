c013bc63eabddcd1f152afb8754c8e4856336512
Stanley Emeh <stanemeh808@gmail.com>
2026-06-08 20:00:40 +0100

    corrected bad importation

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const {register, login, profile} = require("../controllers/userController");
-const authMiddleware = require("../middlewares/authMiddleware");
+const { authMiddleware } = require("../middleware/authMiddleware");
 const router = express.Router();
 
 router.post("/register", register);
